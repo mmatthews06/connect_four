855bbd9a9cfd5ed794d1b28cd36c0f8b685800aa
Michael Matthews <mmatthews@Michaels-MBP.lan>
2021-11-09 03:18:33 -0500

    TDD - fix test before implementation

--- tests/test_game.py
+++ tests/test_game.py
@@ -6,11 +6,11 @@ from connect_four.game_board import GamePiece
 
 def test_game_play():
     moves = [0, 1, 0, 1, 0, 1, 0]
-    def mock_input():
+    def mock_input(s):
         return moves.pop(0)
 
     game = Game()
-    with patch('connect_four.input', mock_input):
+    with patch('connect_four.game.input', mock_input):
         game.play()
 
     assert game.player_won(GamePiece.RED)
