3d15171a2fbf5adbd21bc0392db69e31bcce035f
Michael Matthews <mmatthews@Michaels-MBP.lan>
2021-11-09 01:46:33 -0500

    working neighborhood implementation

--- tests/test_game_board.py
+++ tests/test_game_board.py
@@ -1,11 +1,14 @@
 
+from typing import List, Tuple
+from unittest.mock import call, patch, MagicMock
 
-from unittest.mock import call
-from unittest.mock import patch
-from unittest.mock import MagicMock
-
-from connect_four.game_board import GameBoard
+from connect_four.game_board import Diagonals, GameBoard
 from connect_four.game_board import GamePiece
+
+def play(gameBoard: GameBoard, plays: List[Tuple[int, GamePiece]]):
+    for colIdx, piece in plays:
+        gameBoard.play(piece, colIdx)
+
 
 def test_initial_board() -> None:
     gb = GameBoard()
@@ -42,6 +45,7 @@ def test_play_single():
         [1, 0, 0, 0, 0, 0, 0],
     ]
 
+
 def test_play_full_column():
     gb = GameBoard()
 
@@ -56,6 +60,7 @@ def test_play_full_column():
         [1, 0, 0, 0, 0, 0, 0],
         [1, 0, 0, 0, 0, 0, 0],
     ]
+
 
 def test_play_full_last_column():
     gb = GameBoard()
@@ -79,7 +84,38 @@ def test_neighborhood_bottom_left():
 
     assert neighborhood == [
         [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0],
         []
     ]
+
+
+def test_neighborhood_bottom_left_with_pieces():
+    gb = GameBoard()
+    gb.play(GamePiece.RED, 0)
+    gb.play(GamePiece.RED, 1)
+    gb.play(GamePiece.RED, 2)
+    gb.play(GamePiece.RED, 3)
+
+    neighborhood = gb.neighborhood(5, 0)
+
+    assert neighborhood == [
+        [0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1],
+        []
+    ]
+
+
+def test_diagonals_down():
+    diagonals = Diagonals()
+
+    assert diagonals.get_down_diagonal(0, 4) == []
+    assert diagonals.get_down_diagonal(0, 1) == [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6)]
+
+
+def test_diagonals_up():
+    diagonals = Diagonals()
+
+    assert diagonals.get_up_diagonal(0, 4) == [(0, 4), (1, 3), (2, 2), (3, 1), (4, 0)]
+    assert diagonals.get_up_diagonal(0, 1) == []
